fe28da0e5e9af5411bc729ab69646de0c8140e92
yolanda208 <799327507@qq.com>
2017-05-22 17:49:45 +0800

    修改api部分，添加分组的修改和删除。（使用api添加后，多了_id的属性）

--- api/routes/layer-handler.js
+++ api/routes/layer-handler.js
@@ -44,6 +44,15 @@
         }, _insertLayerGroup);
 
         /**
+         * 删除的图层分组
+         */
+        server.post({
+            path: BASEPATH + '/layer-group/delete',
+            version: '0.0.1'
+        }, _deleteLayerGroup);
+
+
+        /**
          * 获取产品信息列表
          */
         server.get({
@@ -77,6 +86,7 @@
         }, _deleteProjectInfo);
 
 
+        //插入产品信息--增
         function _insertProjectInfo(req, res, next) {
             if (_.isUndefined(req.body)) {
                 return res.end(JSON.stringify({
@@ -101,75 +111,35 @@
             });
         }
 
-        function _getProjectInfoList(req, res, next) {
-            ProjectInfoSchema
-                .find()
-                .exec(function (err, doc) {
-                    var result = {
-                        status: (err) ? err.msg : 'success',
-                        data: doc
-                    };
-                    res.end(JSON.stringify(result));
-                });
-        }
-
-        function _getLayerGroupList(req, res, next) {
-            LayerGroupSchema
-                .find()
-                .exec(function (err, doc) {
-                    var result = {
-                        status: (err) ? err.msg : 'success',
-                        data: doc
-                    };
-                    res.end(JSON.stringify(result));
-                });
-        }
-
-        function _insertLayerGroup(req, res, next) {
+        //删除产品信息 --删
+        function _deleteProjectInfo(req, res, next) {
             if (_.isUndefined(req.body)) {
-                return res.end(JSON.stringify({
-                    status: 'Invalid body',
-                    data: null
-                }));
-            }
-
-            var schema = new LayerGroupSchema();
-            schema.initData(req.body);
-            schema.save(function (err) {
-                if (err) {
-                    return res.end(JSON.stringify({
-                        status: 'Invalid body',
-                        data: null
-                    }));
-                }
-
-                return res.end(JSON.stringify({
-                    status: 'success',
-                    data: null
-                }));
-            });
-        }
-
-
-        //更新图层分组信息
-        function _updateLayerGroup(req, res, next) {
-            if (_.isUndefined(req.body)) {
-                return res.end(JSON.stringify({
-                    status: 'Invalid body',
-                    data: null
-                }));
+                return next('Invalid params');
             }
             var body = req.body;
-
             try {
-                // if (recodeSchema.reportVerify(body)) {}
+                //若ID存在 则对当前ID进行删除
+                if (!_.isUndefined(body._id)) {
+                    //如果body内数据正确
+                    var conditions = {_id: body._id};
+                    ProjectInfoSchema
+                        .remove(conditions, function (err) {
+                            if (err) {
+                                return next(err);
+                            } else {
+                                res.end();
+                            }
+                        });
+                } else {
+                    return next('Invalid body!body should have _id');
+                }
             }
             catch (err) {
-                return next(new DBOptionError(415, err));
+                return next(err);
             }
         }
 
-        //更新产品分组信息
+        //更新产品信息--改
         function _updateProjectInfo(req, res, next) {
 
             if (_.isUndefined(req.body)) {
@@ -216,19 +186,59 @@
             }
         }
 
+        //获取产品信息--查
+        function _getProjectInfoList(req, res, next) {
+            ProjectInfoSchema
+                .find()
+                .exec(function (err, doc) {
+                    var result = {
+                        status: (err) ? err.msg : 'success',
+                        data: doc
+                    };
+                    res.end(JSON.stringify(result));
+                });
+        }
 
-        //删除产品分组信息
-        function _deleteProjectInfo(req, res, next) {
+        //插入产品分组信息--增
+        function _insertLayerGroup(req, res, next) {
             if (_.isUndefined(req.body)) {
-                return next('Invalid params');
+                return res.end(JSON.stringify({
+                    status: 'Invalid body',
+                    data: null
+                }));
+            }
+
+            var schema = new LayerGroupSchema();
+            schema.initData(req.body);
+            schema.save(function (err) {
+                if (err) {
+                    return res.end(JSON.stringify({
+                        status: 'Invalid body',
+                        data: null
+                    }));
+                }
+
+                return res.end(JSON.stringify({
+                    status: 'success',
+                    data: null
+                }));
+            });
+        }
+
+        //删除图层分组信息--删
+        function _deleteLayerGroup(req, res, next) {
+            if (_.isUndefined(req.body)) {
+                return res.end(JSON.stringify({
+                    status: 'Invalid body',
+                    data: null
+                }));
             }
             var body = req.body;
             try {
-                //若ID存在 则对当前ID进行删除
                 if (!_.isUndefined(body._id)) {
                     //如果body内数据正确
                     var conditions = {_id: body._id};
-                    ProjectInfoSchema
+                    LayerGroupSchema
                         .remove(conditions, function (err) {
                             if (err) {
                                 return next(err);
@@ -241,8 +251,74 @@
                 }
             }
             catch (err) {
-                return next(err);
+                return next(new DBOptionError(415, err));
             }
+        }
+
+        //更新图层分组信息--改
+        function _updateLayerGroup(req, res, next) {
+            if (_.isUndefined(req.body)) {
+                return res.end(JSON.stringify({
+                    status: 'Invalid body',
+                    data: null
+                }));
+            }
+            var body = req.body;
+
+
+            try {
+                var layerGroupinfo = new LayerGroupSchema();
+                //内容校验
+                if (layerGroupinfo.reportVerify(body)) {
+                    //如果body内数据正确
+                    var layers = [];
+                    for (var i = 0; i < body.layers.length; i++) {
+                        var m_Layer = {};
+                        m_Layer.index = body.layers[i].index;
+                        m_Layer.layerName = body.layers[i].layerName;
+                        layers.push(m_Layer);
+                    }
+                    console.log(layers);
+
+
+                    LayerGroupSchema.update({
+                        _id: body._id
+                    }, {
+                        $set: {
+                            name: body.name,
+                            pictureUrl: body.pictureUrl,
+                            layers: layers,
+                            type: body.type,
+                            typeName: body.typeName
+                        }
+                    }, function (err) {
+                        res.end();
+                        if (err) {
+                            return next(err);
+                        } else {
+                            return next();
+                        }
+                    });
+                } else {
+                    return next('Invalid body');
+                }
+            }
+            catch (err) {
+                return next(new DBOptionError(415, err));
+            }
+        }
+
+        //获取入产品分组信息--查
+        function _getLayerGroupList(req, res, next) {
+            LayerGroupSchema
+                .find()
+                .exec(function (err, doc) {
+                    var result = {
+                        status: (err) ? err.msg : 'success',
+                        data: doc
+                    };
+                    res.end(JSON.stringify(result));
+                });
         }
 
 
