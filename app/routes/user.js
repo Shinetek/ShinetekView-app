92dfc9a65c754adee2d7076aca02fc17dbbae1ac
yolanda208 <799327507@qq.com>
2017-05-23 19:42:22 +0800

    修改路由为shinetekview

--- app/routes/user.js
+++ app/routes/user.js
@@ -11,7 +11,8 @@
     module.exports = function () {
         var router = new Router();
         //主界面
-        router.route('/worldview').get(_startApp);
+        router.route('/worldview').get(_startRemove);
+        router.route('/shinetekview').get(_startApp);
         //主界面3
         //router.route('/worldview3').get(_startApp3);
         //测试动画demo
@@ -20,6 +21,12 @@
         router.route('/worldview_config').get(_startConfigAPP);
         return router;
     };
+    function _startRemove(req, res, next) {
+        /*  var m_Url = window.location.href;
+         var m_URLNEW = m_Url.replace('worldview', "shinetekview");*/
+        res.send(' URL已经迁移到 /shinetekview\n' + m_URLNEW);
+
+    }
 
     function _startApp(req, res, next) {
         res.sendfile('app/worldview.html');
